d43764112421817237b6490d0c75e221cad2325f
bobtjanitor <bobtjanitor@b5605a22-1cdf-11de-b6aa-3535bc3264a2>
2009-05-08 08:36:22 +0000

    added some documention

--- SampleProject/SampleDataLayer/AuthenticationRequests.cs
+++ SampleProject/SampleDataLayer/AuthenticationRequests.cs
@@ -1,24 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Data.Common;
-using System.Linq;
-using System.Text;
+﻿using System.Data;
 
 namespace SampleDataLayer
 {
+    /// <summary>
+    /// A class the implements the <see cref="IAuthenticationRequests"/> interface and uses a 
+    /// Provider Factory to make authentication requests against any type of database specified in
+    /// your config file (web.config, app.config, machine.config, etc)
+    /// </summary>
     public class AuthenticationRequests :IAuthenticationRequests
     {
         private IDBProviderFactory _userDataProvider = DBUserProviderFactory.Instance;
 
 
+        /// <summary>
+        /// Gets or sets the user data provider mostly this is for testing.
+        /// </summary>
+        /// <value>The user data provider.</value>
         public IDBProviderFactory UserDataProvider
         {
             get { return _userDataProvider; }
             set { _userDataProvider = value; }
         }
 
+        /// <summary>
+        /// Authenticates the user.
+        /// </summary>
+        /// <param name="userName">Name of the user.</param>
+        /// <param name="password">The password.</param>
+        /// <returns></returns>
         public bool AuthenticateUser(string userName, string password)
         {
             bool success = false;

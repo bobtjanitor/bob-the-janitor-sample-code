6714cbffe1a6d9c89bb72d6603c6f2660d8764ca
bobtjanitor <bobtjanitor@b5605a22-1cdf-11de-b6aa-3535bc3264a2>
2009-12-13 05:05:31 +0000

    the first checkin for the javascript drop down project

---
+++ trunk/usingJavascriptToPopulateDropDown/usingJavascriptToPopulateDropDown/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+
+namespace usingJavascriptToPopulateDropDown
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            setValue();
+        }
+
+        protected void buttonGetValue_Click(object sender, EventArgs e)
+        {            
+            output.Text = Request.Form[dropDownList.ID];
+        }
+
+        private void setValue()
+        {
+            string script = string.Format("setpopulateDropdown('{0}',{1});", dropDownList.ClientID, Request.Form[dropDownList.ID]);
+            ClientScript.RegisterStartupScript(typeof(Page),"setDropdownValue",script,true);
+            
+        }
+    }
+}

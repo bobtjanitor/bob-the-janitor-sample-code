d5d687fb323e2e18fe77a97c8100c3ed63f534b2
bobtjanitor <bobtjanitor@b5605a22-1cdf-11de-b6aa-3535bc3264a2>
2010-03-27 23:33:53 +0000

    updates to the simple db project

--- SimpleDBSample/SimpleDBSample/Default.aspx.cs
+++ SimpleDBSample/SimpleDBSample/Default.aspx.cs
@@ -7,15 +7,15 @@ namespace SimpleDBSample
 {
     public partial class _Default : Page
     {
-        private ContactsRequests contactRequestInterface;
+        private IContactsRequests contactRequestInterface;
 
-        public ContactsRequests  ContactRequestInterface
+        public IContactsRequests  ContactRequestInterface
         {
             get
             {
                 if (contactRequestInterface==null)
                 {
-                    contactRequestInterface = new ContactsRequests();
+                    contactRequestInterface = Factories.GetContactRequests();
                 }
                 return contactRequestInterface;
             }
@@ -27,7 +27,17 @@ namespace SimpleDBSample
         
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                GetContactData();
+            }         
+        }
 
+        public void GetContactData()
+        {
+            Contacts contactResults = ContactRequestInterface.GetContacts();
+            gridContacts.DataSource = contactResults;
+            gridContacts.DataBind();
         }
 
         protected void buttonSearch_Click(object sender, EventArgs e)
@@ -35,6 +45,25 @@ namespace SimpleDBSample
             string searchName = textContactName.Text;
             Contacts contactResults = ContactRequestInterface.SearchContactsByName(searchName);
             gridContacts.DataSource = contactResults;
+            gridContacts.DataBind();
+        }
+
+        protected void buttonAddContact_Click(object sender, EventArgs e)
+        {
+            Contact newContact = new Contact();
+            newContact.Name = TextName.Text;
+            newContact.Email = TextEmail.Text;
+            newContact.Phone = TextPhone.Text;
+            ContactRequestInterface.AddContact(newContact);
+
+            GetContactData();
+
+        }
+
+        protected void buttonShowAll_Click(object sender, EventArgs e)
+        {
+            Contacts contactResults = ContactRequestInterface.GetContacts();
+            gridContacts.DataSource = contactResults;            
             gridContacts.DataBind();
         }
     }

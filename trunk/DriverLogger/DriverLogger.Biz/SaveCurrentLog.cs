bf277fd52ee06b88bf076e8e43a11af8650a360d
bobtjanitor <bobtjanitor@b5605a22-1cdf-11de-b6aa-3535bc3264a2>
2010-04-16 01:07:27 +0000

    the driver logger sample app

---
+++ trunk/DriverLogger/DriverLogger.Biz/SaveCurrentLog.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DriverLogger.Biz
+{
+    public class SaveCurrentLog 
+    {
+        public int UserId { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        private IGeoData _geoDataInterface = null;
+        public IGeoData GeoDataInterface
+        {
+            get
+            {
+               return _geoDataInterface;
+            }
+            set
+            {
+                _geoDataInterface = value;
+            }
+        }
+
+        public List<string> Save(Log currentLog)
+        {
+            List<string> errors = new List<string>();
+            if (UserId>0)
+            {
+                errors.AddRange(Validate(currentLog));                
+            }
+            else
+            {
+                errors.Add("Invalid User Id");
+            }
+
+            return errors;
+        }
+
+        public IList<string> Validate(Log currentLog)
+        {
+            List<string> errors = new List<string>();
+            if (string.IsNullOrEmpty(currentLog.Activity))
+            {
+                errors.Add("Activity Required");
+            }
+
+            return errors;
+        }
+
+
+        public Log GetLatLonForCity()
+        {
+            Log result = GeoDataInterface.ReturnCityState(City, State);
+            return result;
+        }
+    }
+
+    public interface IGeoData
+    {
+        Log ReturnCityState(string city, string state);
+    }
+
+    public class Log
+    {
+        public DateTime Date { get; set; }
+        public string Activity { get; set; }
+        public int Lat { get; set; }
+        public int Lon { get; set; }
+    }
+}
---
+++ trunk/DriverLogger/DriverLogger.Biz_NUnit_Test/SaveCurrentLog_Tests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DriverLogger.Biz;
+using NUnit.Framework;
+
+namespace DriverLogger.Biz_NUnit_Test
+{
+    [TestFixture]
+    public class SaveCurrentLog_Tests
+    {
+        private SaveCurrentLog target;
+
+        [SetUp]
+        public void Setup()
+        {
+            target = new SaveCurrentLog();
+        }
+
+        [Test]
+        public void Validate_IfActivityIsEmptyReturnError_Test()
+        {
+            Log testLog = new Log {Activity = "", Date = DateTime.Now, Lat = 100, Lon = -100};
+            var result =  target.Validate(testLog);
+            var acctual = from 
+                              error in result 
+                          where 
+                          error.Contains("Activity Required") 
+                          select error;
+            Assert.AreEqual(1, acctual.Count());
+        }
+
+        [Test]
+        public void GetLatLonForCity_GetTheLatLonForASpecificCity_Test()
+        {
+            target.City = "Boise";
+            target.State = "Id";
+            target.GeoDataInterface = new MockIGeoData();
+
+            Log testLog = target.GetLatLonForCity();
+
+            Assert.AreEqual(100, testLog.Lat); 
+        }
+
+        [Test]
+        public void GetLatLonForCity_GetTheLatLonForASpecificCityCallsGeoInterface_Test()
+        {
+           
+            MockIGeoData testMock = new MockIGeoData();            
+            target.GeoDataInterface = testMock;
+            Log testLog = target.GetLatLonForCity();
+
+            Assert.AreEqual(1, testMock.GeoDataInterfaceCalledCount);
+        }
+
+        public class MockIGeoData : IGeoData
+        {
+            public int GeoDataInterfaceCalledCount=0;
+
+            public Log ReturnCityState(string city, string state)
+            {
+                GeoDataInterfaceCalledCount++;
+                return new Log(){Lat = 100};
+            }
+
+
+        }
+       
+    }
+}

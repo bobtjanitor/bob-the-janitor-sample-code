2b32f31dc00e5a8b9230e12afc0e37ab9b2f9944
bobtjanitor <bobtjanitor@b5605a22-1cdf-11de-b6aa-3535bc3264a2>
2010-04-16 03:06:29 +0000

    added a test project to see if it worked better with .NET 3.5

---
+++ JustMockDemoApp_Tests/ErrorLoger_Tests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web;
+using JustMockDemoApplication;
+using NUnit.Framework;
+using Telerik.JustMock;
+
+namespace JustMockDemoApp_Tests
+{
+    [TestFixture]
+    public class ErrorLoger_Tests
+    {
+        ErrorLoger target;
+        private const string TestError = "Test Error";
+        public MockILogger MockLogger;
+
+        [SetUp]
+        public void Setup()
+        {
+            target = new ErrorLoger();
+            HttpContext mockContext = Mock.Create<HttpContext>();
+            Mock.Arrange(() => mockContext.Error).Returns(new Exception(TestError));
+            target.Context = mockContext;
+            MockLogger = new MockILogger();
+            target.LoggerInterface = MockLogger;
+        }
+       
+       [Test]    
+        public void LogError_SetsLoggerMessageEqualToContextErrorMessage_Test()
+        {
+           target.LogError();
+
+           Assert.AreEqual(TestError,MockLogger.Message);
+        }
+    }
+
+    public class MockILogger: ILogger
+    {
+        public string Message
+        {
+            get; set;
+        }
+    }
+}

3d1aa5dabc803e1d8a30f96c62e1cf4d9ddc18cb
bobtjanitor <bobtjanitor@b5605a22-1cdf-11de-b6aa-3535bc3264a2>
2010-05-13 05:34:12 +0000

    added upload image

--- S3Proof/S3Proof/Default.aspx.designer.cs
+++ S3Proof/S3Proof/Default.aspx.designer.cs
@@ -29,5 +29,23 @@ namespace S3Proof {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl DivlistOfImages;
+        
+        /// <summary>
+        /// imageUpload control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload imageUpload;
+        
+        /// <summary>
+        /// buttonuploadImage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button buttonuploadImage;
     }
 }

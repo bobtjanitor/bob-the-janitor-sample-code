bd34ece469e8e0ebd77cd1484098623e0a496df1
bobtjanitor <bobtjanitor@b5605a22-1cdf-11de-b6aa-3535bc3264a2>
2010-05-19 13:21:43 +0000

    Added a redirect after login tied the Authentcation biz layer to the UI login control now does a sudo login

---
+++ trunk/cyclingLog/cyclingLog/Factories/AuthenticationFactory.cs
@@ -0,0 +1,12 @@
+﻿using cyclingLog.Biz;
+
+namespace cyclingLog.Factories
+{
+    public static class AuthenticationFactory
+    {
+        public static IAuthentication GetAuthentication()
+        {
+            return new Authentication();
+        }
+    }
+}

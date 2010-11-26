6d434131ee959887d874e84b44119a8edfb652aa
bobtjanitor <bobtjanitor@b5605a22-1cdf-11de-b6aa-3535bc3264a2>
2010-11-26 23:57:13 +0000

    rhomoble Project

---
+++ routingWebServiceProof/SearchingProof/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SearchingProof.Searching;
+
+namespace SearchingProof
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoadSearchClient client = new LoadSearchClient();
+            LoadSearchRequest request = new LoadSearchRequest();
+            request.Criteria = new LoadSearchCriteria(){OriginCity = "Boise",OriginState = "ID",EquipmentType = "V"};
+            request.Criteria.LoadType = LoadType.All;
+            request.IntegrationId = 4;
+            request.UserName = "TestUser";
+            request.Password = "tupass123";
+
+            LoadSearchReturn result = client.GetLoadSearchResults(request);
+            Console.WriteLine(result.Errors.Count());
+            Console.WriteLine(result.SearchResults.Count());
+        }
+    }
+}

32696f3de5967157b8bdf562a857a19c2c40e211
bobtjanitor <bobtjanitor@b5605a22-1cdf-11de-b6aa-3535bc3264a2>
2010-12-02 08:14:47 +0000

    the ajax made easy proof

---
+++ ajaxMadeEasy/ajaxMadeEasy/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Web.Mvc;
+using ajaxMadeEasy.Models;
+
+namespace ajaxMadeEasy.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            UserModel userModel = new UserModel(){UserId = 1,Type = UserTypes.User};
+
+            return View(userModel);
+        }
+
+        [AcceptVerbs(WebRequestMethods.Http.Post)]
+        public ActionResult UpdateUserType(UpdateUser input)
+        {
+            var result = new {Succss = true, Message = "Updated"};
+            return Json(result);
+       }
+    }
+}

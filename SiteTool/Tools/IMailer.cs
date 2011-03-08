1214cd35a5bf80583b966ca9437e867daa09c674
bobtjanitor <bobtjanitor@gmail.com>
2011-03-08 08:37:30 +0000

    the checkin for Site Tools

---
+++ SiteTool/Tools/IMailer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Tools
+{
+    public interface IMailer
+    {
+        string Subject { get; set; }
+        string To { get; set; }
+        string From { get; set; }
+        string Body { get; set; }
+        IList<string> Errors { get; set; }
+        bool SendMail();
+    }
+}
---
+++ SiteTool/Tools/Mailer.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Net.Mail;
+
+namespace Tools
+{
+    public class Mailer : IMailer
+    {
+        public string Subject { get; set; }
+        public string To { get; set; }
+        public string From { get; set; }
+        public string Body { get; set; }
+        public IList<string> Errors { get; set; }
+
+        private SmtpClient _smtpClient;
+        public SmtpClient SmtpClient
+        {
+            get
+            {
+                if (_smtpClient==null)
+                {
+                    _smtpClient = new SmtpClient();
+                }
+                return _smtpClient;
+            }
+            set { _smtpClient = value; }
+        }
+
+        private string _smtpServer;
+        public string SmtpServer
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_smtpServer))
+                {
+                    _smtpServer = ConfigurationManager.AppSettings["SmtpServer"];
+                }
+                return _smtpServer;
+            }
+            set { _smtpServer = value; }
+        }
+
+        private string _smtpUser;
+        public string SmtpUser
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_smtpUser))
+                {
+                    _smtpUser = ConfigurationManager.AppSettings["SmtpUser"];
+                }
+                return _smtpUser;
+            }
+            set { _smtpUser = value; }
+        }
+
+        private string _smtpPassword;
+        public string SmtpPassword
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_smtpPassword))
+                {
+                    _smtpPassword = ConfigurationManager.AppSettings["SmtpPassword"];
+                }
+                return _smtpPassword;
+            }
+            set { _smtpPassword = value; }
+        }
+
+        public bool SendMail()
+        {
+            
+            return false;
+        }
+    }
+}

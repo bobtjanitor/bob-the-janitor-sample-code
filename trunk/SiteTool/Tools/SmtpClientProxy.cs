dfc6de6ed023b92655d7267b2fa5e2ef9d14292b
bobtjanitor@gmail.com <bobtjanitor@gmail.com@b5605a22-1cdf-11de-b6aa-3535bc3264a2>
2011-03-11 05:24:56 +0000

    finished the Mailer for Site tools, with tests

---
+++ trunk/SiteTool/Tools/SmtpClientProxy.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace Tools
+{
+    public class SmtpClientProxy : SmtpClient, IEmailClientProxy
+    {       
+        public new void Send(MailMessage message)
+        {
+            base.Send(message);
+        }
+
+        public new ICredentialsByHost Credentials
+        {
+            get { return base.Credentials; }
+            set { base.Credentials = value; }
+        }
+    }
+}
---
+++ trunk/SiteTool/Tools_Tests/ClientFactory_Tests.cs
@@ -0,0 +1,17 @@
+﻿using Tools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tools_Tests
+{
+    [TestClass()]
+    public class ClientFactory_Tests
+    {
+        [TestMethod()]
+        public void GetEmailClient_ReturnExpectedType_Test()
+        {
+            IEmailClientProxy actual = ClientFactory.GetEmailClient();
+            Assert.IsInstanceOfType(actual, typeof(SmtpClientProxy));
+        }
+    }
+}

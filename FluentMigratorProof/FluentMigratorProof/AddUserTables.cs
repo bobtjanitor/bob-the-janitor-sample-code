55e6c180a4283715620c47522930a572984917ed
bobtjanitor <bobtjanitor@gmail.com>
2011-07-17 05:35:53 +0000

    the finnished fluentmigrator proof project

---
+++ FluentMigratorProof/FluentMigratorProof/AddUserTables.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace FluentMigratorProof
+{
+    [Migration(20110624232410)]
+    public class AddUserTables : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Users")
+                .WithColumn("Id").AsInt32().Identity().NotNullable()
+                .WithColumn("Name").AsString().NotNullable()
+                .WithColumn("Password").AsAnsiString().NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Users");
+        }
+    }
+}
---
+++ FluentMigratorProof/FluentMigratorProof/DevelopmentProfile.cs
@@ -0,0 +1,34 @@
+﻿using FluentMigrator;
+
+namespace FluentMigratorProof
+{
+    [Profile("Development")]
+    public class DevelopmentProfile : Migration
+    {
+        public override void Up()
+        {
+
+            Insert.IntoTable("Users").Row(new { Name = "TestUser", Password = "12345" });
+        }
+
+        public override void Down()
+        {
+            Execute.Sql("DELETE FROM `Users` WHERE Name = 'TestUser'");
+        }
+    }
+
+    [Profile("Production")]
+    public class ProductionProfile : Migration
+    {
+        public override void Up()
+        {
+
+            
+        }
+
+        public override void Down()
+        {
+            
+        }
+    }
+}

5a5479a98e1c40871c786137b0952bbb65bc7afc
bobtjanitor@gmail.com <bobtjanitor@gmail.com@b5605a22-1cdf-11de-b6aa-3535bc3264a2>
2011-07-28 14:04:00 +0000

    added some behavioral tests

---
+++ trunk/SampleApplication/SampleApplication.Domain_Tests/Behavioral/When_Requesting_Employees_From_Seattle_Washington_Tests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using SampleApplication.Objects.Dvrs;
+using SampleApplication.Repository;
+
+namespace SampleApplication.Domain_Tests.Behavioral
+{
+    [TestFixture]
+    public class When_Requesting_Employees_From_Seattle_Washington_Tests : EmployeeRequests_Context
+    {
+        public override void Context()
+        {
+            base.Context();
+            MockEmployeeRepository = new Mock<IEmployeeRepository>();
+            MockEmployeeRepository.Setup(x => x.GetEmployeeByCityState("Seattle", "Washington")).Returns<List<EmployeeDvr>>(
+                x => new List<EmployeeDvr>() { new EmployeeDvr() { City = "Seattle", State = "Washington" } });
+            Target.EmployeeRepository = MockEmployeeRepository.Object;
+        }
+
+        public override void Becouse()
+        {
+            Target.GetEmployeeByLocation("Seattle", "Washington");
+        }
+
+        [Test]
+        public void HasNoErrors_Test()
+        {
+            Assert.AreEqual(0,Target.Errors.Count);
+        }        
+    }
+}

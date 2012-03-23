c8e2118f63f55c369d36425558fd2e947a76ea6e
bobtjanitor <bobtjanitor@b5605a22-1cdf-11de-b6aa-3535bc3264a2>
2012-03-23 06:21:44 +0000

    did some more work on the sample app for code camp

---
+++ SampleApplication/SampleApplication/Controllers/BasicDemoController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using SampleApplication.Models;
+
+namespace SampleApplication.Controllers
+{
+    public class BasicDemoController : Controller
+    {
+        public ActionResult Index()
+        {
+            BasicDemoModel model = new BasicDemoModel();
+            //model.City = "Boise";
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(BasicDemoModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Message = "It's All Good";
+            }
+            else
+            {
+                model.Message = "Something went wrong";
+            }            
+            return View(model);
+        }
+    }
+}
---
+++ SampleApplication/SampleApplication/Models/BasicDemoModel.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleApplication.Models
+{
+    public interface IDemoModel: IAddressModel
+    {
+        string Name { get; set; }
+        string Email { get; set; }
+        string Message { get; set; }
+    }
+
+    public class BasicDemoModel : IDemoModel
+    {
+        public int Id;
+        [Required]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "You must have an email address")]
+        public string Email { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]        
+        public string State { get; set; }
+        [Required]
+        [RegularExpression(@"^\d[5]$", ErrorMessage = "Zip Code must have exactly 5 numbers")]
+        [Display(Name = "Zip")]
+        public int ZipCode { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class GermancDemoModel : IDemoModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Sie müssen ein email address haben")]
+        [Display(Name = "eMail")]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "Adresse")]
+        public string Address { get; set; }
+        [Required]
+        [Display(Name = "Stadt")]
+        public string City { get; set; }
+        [Required]
+        [Display(Name = "Zustand")]
+        public string State { get; set; }
+        [Required]
+        [RegularExpression(@"^\d[5]$", ErrorMessage = "Zip Code must have exactly 5 numbers")]
+        [Display(Name = "Postleitzahl")]
+        public int ZipCode { get; set; }
+
+        public string Message { get; set; }
+    }
+
+    public interface IAddressModel
+    {
+        string Address { get; set; }
+        string City { get; set; }
+        string State { get; set; }
+        int ZipCode { get; set; }
+    }
+}

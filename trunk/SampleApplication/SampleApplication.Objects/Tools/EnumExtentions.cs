0fae9a342f327d7927b1083bed452317c1d0a331
bobtjanitor@gmail.com <bobtjanitor@gmail.com@b5605a22-1cdf-11de-b6aa-3535bc3264a2>
2012-04-30 04:18:41 +0000

    cleaned up code

--- trunk/SampleApplication/SampleApplication.Objects/Tools/EnumExtentions.cs
+++ trunk/SampleApplication/SampleApplication.Objects/Tools/EnumExtentions.cs
@@ -1,30 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Reflection;
-using System.Text;
 
 namespace SampleApplication.Objects.Tools
 {
     public static class EnumExtentions
     {
-public static string ToDiscription(this Enum value)
-{
-    var description = string.Empty;
-    FieldInfo fi = value.GetType().GetField(value.ToString());
+        public static string ToDiscription(this Enum value)
+        {
+            var description = string.Empty;
+            FieldInfo fi = value.GetType().GetField(value.ToString());
 
-    var attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute),false);
+            var attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute),false);
 
-    if (attributes != null && attributes.Length > 0)
-    {
-        description = attributes[0].Description;
-    }
-    else
-    {
-        description = value.ToString();
-    }
-    return description;
-}
+            if (attributes != null && attributes.Length > 0)
+            {
+                description = attributes[0].Description;
+            }
+            else
+            {
+                description = value.ToString();
+            }
+            return description;
+        }
     }
 }

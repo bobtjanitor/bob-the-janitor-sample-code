3a5fb5596b383a7dcd85dee7baf3a49fbe12519e
bobtjanitor@gmail.com <bobtjanitor@gmail.com@b5605a22-1cdf-11de-b6aa-3535bc3264a2>
2012-05-18 22:39:42 +0000

    first commit

---
+++ trunk/NetAdvantageProofs/NetAdvantageProofs/Models/ProductModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NetAdvantageProofs.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ProductNumber { get; set; }
+    }
+}

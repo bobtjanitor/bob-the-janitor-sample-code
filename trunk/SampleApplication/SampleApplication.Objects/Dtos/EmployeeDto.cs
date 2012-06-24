f8a3a62b22897f2bc195ad8bfa6368bc010f09ef
bobtjanitor@gmail.com <bobtjanitor@gmail.com@b5605a22-1cdf-11de-b6aa-3535bc3264a2>
2012-06-24 07:22:14 +0000

    add tests and mrthods for doing a blog post on testing sequance

--- trunk/SampleApplication/SampleApplication.Objects/Dtos/EmployeeDto.cs
+++ trunk/SampleApplication/SampleApplication.Objects/Dtos/EmployeeDto.cs
@@ -1,13 +1,13 @@
-﻿using System;
-
-namespace SampleApplication.Objects.Dvrs
+﻿namespace SampleApplication.Objects.Dtos
 {
-    public class EmployeeDvr
+    public class EmployeeDto
     {
         public string City { get; set; }
 
         public string State { get; set; }
 
         public string EmployeeName { get; set; }
+
+        public int Id { get; set; }
     }
 }

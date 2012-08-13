7a8492b7aa4b52eea0d698d0e22e33cf3cb44c28
bobtjanitor@gmail.com <bobtjanitor@gmail.com@b5605a22-1cdf-11de-b6aa-3535bc3264a2>
2012-08-13 04:12:09 +0000

    first check in for the dev spike

---
+++ trunk/TestFrameworkSpike/TestSpike/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TestRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length==0)
+            {
+                args = new []{"Sample_Tests.dll"};
+            }
+            
+            foreach (var s in args)
+            {
+                var builder = new TestBuilder(s);
+                foreach (var result in builder.TestResultses)
+                {
+                    Console.WriteLine(result.Description);
+                    for (int x=0; x<result.TestResultMesssage.Count;x++)
+                    {
+                        var testResult = result.TestResultMesssage[x];
+                        var current = x + 1;
+                        var total = result.TestResultMesssage.Count;
+
+                        if (testResult.Success)
+                        {
+                            Console.WriteLine(string.Format("{0}\t success {1}/{2}", testResult.TestName, current, total));
+                        }
+                        else
+                        {
+                            Console.WriteLine(string.Format("{0}\t Failed {1}/{2}", testResult.TestName, current, total));
+                            Console.WriteLine(testResult.Result);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("press any key");
+            Console.ReadKey();
+        }
+    }
+}

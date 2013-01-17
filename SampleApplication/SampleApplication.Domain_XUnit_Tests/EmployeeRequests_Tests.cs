7b171427676407795501ae491508a5efc3d8805b
bobtjanitor <bobtjanitor@gmail.com>
2013-01-17 07:21:03 +0000

    Adding test projects for MS Test and Xunit

---
+++ SampleApplication/SampleApplication.Domain_XUnit_Tests/EmployeeRequests_Tests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Moq;
+using SampleApplication.Domain;
+using SampleApplication.Objects.Dtos;
+using SampleApplication.Objects.RepositoryInterfaces;
+using Xunit;
+
+namespace SampleApplication.Domain_XUnit_Tests
+{
+    public class EmployeeRequests_Tests
+    {
+        private EmployeeRequests target = new EmployeeRequests();
+        private Mock<IEmployeeRepository> MockEmployeeRepository = new Mock<IEmployeeRepository>();
+        private IList<EmployeeDto> Expected = new List<EmployeeDto>{new EmployeeDto(), new EmployeeDto()};
+
+        public void Action(string city, string state)
+        {
+            MockEmployeeRepository.Setup(x=>x.GetEmployeeByCityState(city,state)).Returns(Expected);
+            target.EmployeeRepository = MockEmployeeRepository.Object;
+            target.GetEmployeeByLocation(city, state);
+        }
+
+        [Fact]
+        public void Calls_EmployeeRepository_Test()
+        {
+            var city = "TestCity";
+            var state = "TestState";
+            Action(city,state);
+            MockEmployeeRepository.Verify(x=>x.GetEmployeeByCityState(city,state),Times.Once());
+        }
+
+    }
+}
---
+++ SampleApplication/SampleApplication_MSTest/EmployeeRequests_Tests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SampleApplication.Domain;
+using SampleApplication.Objects.Dtos;
+using SampleApplication.Objects.RepositoryInterfaces;
+
+namespace SampleApplication_MSTest
+{
+    [TestClass]
+    public class EmployeeRequests_Tests
+    {
+        public EmployeeRequests Target;
+        public Mock<IEmployeeRepository> EmployeeRepositoryMock;
+        public string ExpectedCity;
+        public string ExpectedState;
+        public IList<EmployeeDto> Expected;
+        public IList<EmployeeDto> Actual;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            ExpectedCity = "TestCity";
+            ExpectedState = "Id";
+            Expected = new List<EmployeeDto>{new EmployeeDto(), new EmployeeDto(), new EmployeeDto()};
+            EmployeeRepositoryMock = new Mock<IEmployeeRepository>();
+            EmployeeRepositoryMock.Setup(x => x.GetEmployeeByCityState(ExpectedCity, ExpectedState))
+                                  .Returns(Expected);
+            Target = new EmployeeRequests { EmployeeRepository = EmployeeRepositoryMock.Object };
+            Actual = Target.GetEmployeeByLocation(ExpectedCity, ExpectedState);
+        }
+
+        [TestMethod]
+        public void Calls_EmployeeRepository_GetEmployeeByCityState_Test()
+        {
+            EmployeeRepositoryMock.Verify(x => x.GetEmployeeByCityState(ExpectedCity, ExpectedState), Times.Once());
+        }
+    }
+}

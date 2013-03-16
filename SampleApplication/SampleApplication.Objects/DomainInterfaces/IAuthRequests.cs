21edd84a1041fe493db11d915527eafab7183f31
bobtjanitor <bobtjanitor@gmail.com>
2013-03-16 19:41:46 +0000

    adding test data

--- SampleApplication/SampleApplication.Objects/DomainInterfaces/IAuthRequests.cs
+++ SampleApplication/SampleApplication.Objects/DomainInterfaces/IAuthRequests.cs
@@ -1,7 +1,15 @@
-﻿namespace SampleApplication.Objects.DomainInterfaces
+﻿using System.Collections.Generic;
+using SampleApplication.Objects.Dtos;
+
+namespace SampleApplication.Objects.DomainInterfaces
 {
     public interface IAuthRequests
     {
         bool UserCanUpdateEmployee(int userId, int employeeId);
+    }
+
+    public interface IEmployeeRequests
+    {
+        IList<EmployeeDto> GetEmployeeByLocation(string city, string state);
     }
 }

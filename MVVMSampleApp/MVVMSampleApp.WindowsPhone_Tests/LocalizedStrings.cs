4da258b07a8f5518a61f6516b262223c444e6d5c
bobtjanitor <bobtjanitor@gmail.com>
2014-03-28 22:03:36 +0000

    Adding the windows phone and store plus tests

---
+++ MVVMSampleApp/MVVMSampleApp.WindowsPhone_Tests/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using MVVMSampleApp.WindowsPhone_Tests.Resources;
+
+namespace MVVMSampleApp.WindowsPhone_Tests
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
